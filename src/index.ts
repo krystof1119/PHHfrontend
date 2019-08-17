40348c15a2848bc7ef01bd52db42579cf7284c62
Kryštof Píštěk <krystof1119@gmail.com>
2019-08-17 20:54:17 +0200

    Test all the configs with a simple "hello world"

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-alert(1)
+function index() {
+    console.log('a');
+    let reactContainer: HTMLElement = document.getElementById('react-container');
+    console.log(reactContainer);
+    reactContainer.innerText = 'ayy it __really__ actually works';
+}
+
+addEventListener('load', index);
