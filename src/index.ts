fa9e77a9ba6d1c74ad680e3b21ff4f27613fa615
Kryštof Píštěk <krystof1119@gmail.com>
2019-08-22 18:48:28 +0200

    Import everything for index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,22 @@
+import * as ReactDOM from 'react-dom';
+import MainComponent from "./react-1/main";
+
+import './angular/main';
+import '@angular/common';
+import '@angular/core';
+import '@angular/platform-browser';
+import '@angular/platform-browser-dynamic';
+import '@angular/compiler';
+import 'rxjs';
+import 'rxjs-compat';
+import 'rxjs/operators';
+
+// What does everything need? Yup, you guessed it, say it with me
+import 'jquery';
+// Because of course it does
+
+import 'bootstrap';
+
 function index() {
     console.log('a');
     let reactContainer: HTMLElement = document.getElementById('react-container');
