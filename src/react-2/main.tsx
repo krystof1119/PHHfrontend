506ab9fe7e403689aa2af95dc566cf127f69fed6
Kryštof Píštěk <krystof1119@gmail.com>
2019-08-29 19:18:34 +0200

    Add state, <ManualAdder /> and some text to react-2's MainComponent

--- src/react-2/main.tsx
+++ src/react-2/main.tsx
@@ -1,11 +1,17 @@
 import * as React from 'react';
 import RainbowButton from "./rainbowButton";
+import ManualAdder from "./manualAdder";
+import {useState} from "react";
 
 export default function MainComponent() {
+    let [currentInput, setCurrentInput] = useState('');
     return (
         <div>
             <a className="disappearing">React instance 2 loaded</a>
-            <RainbowButton onClick={color =>console.log(String.fromCodePoint(color))} />
+            <h5 style={{textAlign: 'center'}}>Don't know what to do here? Press the help button above this message.</h5>
+            <RainbowButton onClick={color => setCurrentInput(currentValue => currentValue + String.fromCodePoint(color))} />
+            <p>Currently, your input is: "{currentInput}"</p>
+            <ManualAdder onClick={string => setCurrentInput(currentValue => currentValue + string)}/>
         </div>
     )
 }
