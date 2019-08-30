4f5757a0295269608b0db37faef20617712e5ebd
Kryštof Píštěk <krystof1119@gmail.com>
2019-08-30 10:36:53 +0200

    Put <Help /> inside <MainComponent /> or react-1

--- src/react-1/main.tsx
+++ src/react-1/main.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import VueDiv from "./vueDiv";
+import Help from "./help";
 
 export default function MainComponent() {
 
     return (
         <div>
             <a className="disappearing">React instance 1 loaded</a>
+            <Help />
             <VueDiv/>
         </div>
     )
