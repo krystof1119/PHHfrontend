c7bec0bb8d1bbd35790367b5efe884fdc65866ec
Kryštof Píštěk <krystof1119@gmail.com>
2019-08-31 13:16:49 +0200

    Refactor MainComponent of react-2 into a class, and query on Submit click. Also refactor the output into a textarea.

--- src/react-2/main.tsx
+++ src/react-2/main.tsx
@@ -1,35 +1,68 @@
 import * as React from 'react';
 import RainbowButton from "./rainbowButton";
 import ManualAdder from "./manualAdder";
-import {useState} from "react";
-import {useQuery} from "@apollo/react-hooks";
 import {gql} from "apollo-boost";
+import ApolloClient from "apollo-client";
+import {withApollo} from 'react-apollo';
 
-export default function MainComponent() {
-    let [currentInput, setCurrentInput] = useState('');
-    let [currentVar, setCurrentVar] = useState('');
-    const {data, loading} = useQuery(gql`
-    query getData($var: String!, $browserId: String!){
-        res(var: $var, browserId: $browserId)
+class MainComponent extends React.Component<{ client: ApolloClient<object> }, { currentInput: string, output: string }> {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentInput: '',
+            output: ''
+        };
+
+        this.setCurrentInput = this.setCurrentInput.bind(this);
+        this.doQuery = this.doQuery.bind(this);
     }
-    `, {variables: {var: currentVar, browserId: localStorage.getItem('browserId')}});
-    return (
-        <div>
-            <a className="disappearing">React instance 2 loaded</a>
-            <h5 style={{textAlign: 'center'}}>Don't know what to do here? Press the help button above this message.</h5>
-            <RainbowButton onClick={color => setCurrentInput(currentValue => currentValue + String.fromCodePoint(color))} />
-            <p>Currently, your input is: "{currentInput}"</p>
-            <a>Your output is: "{loading ? '' : data.res}"</a>
-            <ManualAdder onClick={string => setCurrentInput(currentValue => currentValue + string)}/>
-            <br />
-            <div className={'row'}>
-                <button className={'col-sm btn btn-primary btn-lg btn-block'} onClick={() => {setCurrentVar(currentInput);setCurrentInput('')}}>
-                    Submit
-                </button>
-                <button className={'col-sm btn btn-secondary btn-lg btn-block'} onClick={() => setCurrentInput('')}>
-                    Reset
-                </button>
+
+    setCurrentInput(val: string) {
+        this.setState({
+            currentInput: val
+        })
+    }
+
+    doQuery() {
+        this.props.client.query({
+            query: gql`query getData($var: String!, $browserId: String!){
+                res(var: $var, browserId: $browserId)
+            }`, variables: {var: this.state.currentInput, browserId: localStorage.getItem('browserId')}
+        })
+            .then(res => {
+                console.log(res.data.res);
+                this.setState({output: res.data.res})
+            });
+    }
+
+    render() {
+        return (
+            <div>
+                <a className="disappearing">React instance 2 loaded</a>
+                <h5 style={{textAlign: 'center'}}>Don't know what to do here? Press the help button above this
+                    message.</h5>
+                <RainbowButton
+                    onClick={color => this.setCurrentInput(this.state.currentInput + String.fromCodePoint(color))}/>
+                <p>Currently, your input is: "{this.state.currentInput}"</p>
+                <textarea disabled value={this.state.output} className={'form-control purple-border'} rows={5}/>
+                <br/>
+                <ManualAdder onClick={string => this.setCurrentInput(this.state.currentInput + string)}/>
+                <br/>
+                <div className={'row'}>
+                    <button className={'col-sm btn btn-primary btn-lg btn-block'} onClick={() => {
+                        this.doQuery();
+                        this.setCurrentInput('')
+                    }}>
+                        Submit
+                    </button>
+                    <button className={'col-sm btn btn-secondary btn-lg btn-block'}
+                            onClick={() => this.setCurrentInput('')}>
+                        Reset
+                    </button>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
+
+export default withApollo(MainComponent);
